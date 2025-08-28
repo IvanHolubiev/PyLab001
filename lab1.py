64a4e4dc90c615fabdbc9f2233267da110ee1f91
iholubiev454 <vanyagolubev7@gmail.com>
2025-08-28 12:33:27 -0700

    Finished lab1

---
+++ lab1.py
@@ -0,0 +1,37 @@
+# import math
+from math import pi, sqrt
+
+radius = 5
+area = pi * radius**2
+print(area)
+
+radius = 3
+vol_sphere = 4/3 * pi * radius**3
+print(vol_sphere)
+
+side_a = 3
+side_b = 4
+side_c_sq = side_a**2 + side_b**2
+side_c = int(sqrt(side_c_sq))
+print(side_c)
+
+first_name = "Ivan"
+last_name = "Holubiev"
+name_len = len(first_name) + len(last_name)
+print(name_len)
+
+my_name = first_name +" "+ last_name
+print(my_name)
+
+my_name_uc = my_name.upper()
+my_name_lc = my_name.lower()
+print(my_name_uc, my_name_lc)
+
+age = 19
+weight = 121.0
+height = 65
+
+print(type(age), type(weight), type(height))
+
+bmi = weight / height**2 * 703
+print(bmi)
